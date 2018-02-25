50717af3f9d4f33d424a85cdab10b1c16d16cc76
CChristiansen23 <christiansenconner@gmail.com>
2018-02-25 15:53:15 -0700

    Add files via upload

---
+++ Server.vue
@@ -0,0 +1,21 @@
+<template>
+    <li
+            class="list-group-item"
+            style="cursor: pointer"
+            @click="serverSelected">
+        Server #{{ server.id }}
+    </li>
+</template>
+
+<script>
+    import { eventBus } from '../main.js';
+
+    export default {
+        props: ['server'],
+        methods: {
+            serverSelected() {
+                eventBus.$emit('serverSelected', this.server);
+            }
+        }
+    }
+</script>
---
+++ ServerDetails.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!server">Please Select A Server</p>
+        <p v-else>Server #{{ server.id }} selected, Status: {{ server.status }}</p>
+        <hr>
+        <button @click="resetStatus">Change to Normal</button>
+    </div>
+
+</template>
+
+<script>
+    import { eventBus } from '../main.js';
+
+    export default {
+        data: function() {
+            return {
+                server: null
+            }
+        },
+        methods: {
+           resetStatus() {
+               this.server.status = "Normal";
+           }
+        },
+        created() {
+            eventBus.$on('serverSelected', (server) => {
+                this.server = server;
+            })
+        }
+    }
+</script>
+
+<style>
+
+</style>
